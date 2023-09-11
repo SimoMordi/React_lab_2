e983f84a005b6ff5175b04b08053c225f1466265
simo <simo.mordi@gmail.com>
2023-09-11 12:30:07 -0700

    new

---
+++ src/components/Navbar/NavbarButton/index.jsx
@@ -0,0 +1,18 @@
+import "./index.css"
+
+const NavbarButton = () => {
+
+
+  return (
+    <div className="container">
+
+      <button className="nav">Home</button>
+      <button className="nav">Option</button>
+      <button className="nav">Selection</button>
+      <button className="nav">Function</button>
+
+    </div>
+  )
+}
+
+export default NavbarButton
